b4efd9df7ead44263f43abb681b8fe53f8dc0f95
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-08-30 10:19:04 +0900

    Initial Commit

---
+++ SensorAndBLE/SensorAndBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorAndBLEApp.swift
+//  SensorAndBLE
+//
+//  Created by cmStudent on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct SensorAndBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
